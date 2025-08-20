63eca36d225a03e126008ccdffacc98fc703ef93
sayyad0591-sketch <sayyad0591@gmail.com>
2025-08-20 11:11:43 +0530

    Update test_addition.py

--- src/test_addition.py
+++ src/test_addition.py
@@ -1,4 +1,4 @@
-from src.addition import add  # Assuming you have a function named `add`
+from addition import addition  # Assuming you have a function named `add`
 
 def test_add():
     assert add(2, 3) == 5
